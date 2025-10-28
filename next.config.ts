36415b848d5bbbb643e3a1895a64a5499a7f2cab
Tony Alberti <tonymendesspfc@outlook.com>
2025-10-28 18:48:21 -0300

    fix: basePath/assetPrefix + .nojekyll + deploy out/

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
+// next.config.ts
 const isProd = process.env.NODE_ENV === "production";
 const repo = "SitePortifolio";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
